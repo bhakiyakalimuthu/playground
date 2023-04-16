15525a49ff6b9348111317a4e78c477ed09329d2
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2023-04-16 09:40:41 +0200

    lock free and wait free

---
+++ go_lock_free_wait_free/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+// Lock free data structure
+type counterLF struct {
+	value int64
+}
+
+func (cl *counterLF) Inc() {
+	atomic.AddInt64(&cl.value, 1)
+}
+
+func (cl *counterLF) Value() int64 {
+	return atomic.LoadInt64(&cl.value)
+}
+
+// Wait free data structure
+type counterWF struct {
+	value atomic.Value
+}
+
+func (cw *counterWF) Inc() {
+	cw.value.Store(cw.value.Load().(int) + 1)
+}
+
+func (cw *counterWF) Value() int {
+	return cw.value.Load().(int)
+}
+
+func main() {
+	var val int
+	val = 10
+	// lock free
+	lf := &counterWF{val.(atomic.Value)}
+	lf.Inc()
+	lf.Inc()
+	lf.Inc()
+	fmt.Println(lf.Value(), "\n")
+
+	// wait free
+	wf := &counterWF{}
+	wf.Inc()
+	wf.Inc()
+	wf.Inc()
+	wf.Inc()
+	fmt.Println(wf.Value())
+}
